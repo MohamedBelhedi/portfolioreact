73f3628becd6b0f64bc56a78e3884472adabe2f3
Mohamed Belhedi <mbelhedi@outlook.com>
2022-09-05 23:15:22 +0200

    bissschen neue hooks reingemacht, und was ausgebssert

--- src/App.js
+++ src/App.js
@@ -16,15 +16,24 @@ import { AnimationOnScroll } from 'react-animation-on-scroll'
 import ReactAnime from 'react-animejs'
 import meine_Daten from './daten/daten';
 import Whatsapp from './components/whatsapp';
-import { createElement } from 'react';
+import React, {useState, createElement } from 'react';
 import { momo } from './momo';
 
 // eine Such Leiste wo der user nach mein Name und meine Alter etc das als JSON datei und mit fetch un then aufrufen
 const {Anime, stagger} = ReactAnime
 
 function App() {
-
+const [btnText,setBtnText]=useState("durchsuchen")
+const [suchTxt,setSuchTxt]=useState("")
 const testText="Mein Portfolio Web App & Gamedeveloper"
+const chngBtnText=()=>{
+
+setBtnText("suchen....")
+setTimeout(() => {
+ setBtnText(btnText)
+},3000);
+
+}
 const anime=()=>{
 
   const animeTest=document.querySelector('h1')
@@ -73,6 +82,8 @@ const suche=async(e)=>{
     
   
   if(e.key==="Enter"){
+chngBtnText(9)
+ 
     if(input.includes("name")){
 
 const test2=document.querySelector('.test2')
@@ -153,15 +164,135 @@ bildApi.remove();
 
 
  }
-}else if(input!=="name"){
+}else if(input.length===0){
   let test2=document.querySelector('.test2')
   
   
-      test2.innerHTML="leider nicht vorhanden"
+      test2.innerHTML="Bitte etwas eingeben"
       test2.style.transition="all 2s ease-in-out"
       test2.style.color="white"
     
     }
+
+  
+})
+  
+
+
+
+
+}
+const sucheOnClick=async(e)=>{
+
+
+
+
+  
+
+ await Promise.resolve(momo)
+.then(res=>{
+  const input=document.querySelector('input').value
+
+  input.toLowerCase()
+
+//    {input!=="name"?setSuchTxt("Leider nicht vorhanden"):true}
+
+   
+    
+ 
+
+    if(input.includes("name")){
+
+const test2=document.querySelector('.test2')
+test2.innerHTML=res[0].name
+test2.style.color="yellow"
+console.log(res[0].name)
+
+  }
+ if(input.includes("alter"))
+ {
+  const test2=document.querySelector('.test2')
+test2.innerHTML=res[0].alter
+test2.style.transition="all 2s ease-in-out"
+test2.style.color="yellow"
+
+console.log(res[0].alter)
+ }
+ if(input.includes("beruf"))
+ {
+  const test2=document.querySelector('.test2')
+test2.innerHTML=res[0].Beruf
+test2.style.transition="all 2s ease-in-out"
+test2.style.color="yellow"
+console.log(res[0].Beruf)
+ }
+ if(input.includes("hobby"))
+ {
+  const test2=document.querySelector('.test2')
+test2.innerHTML=res[0].hobby
+test2.style.transition="all 2s ease-in-out"
+test2.style.color="yellow"
+console.log(res[0].hobby)
+const bildApi=document.querySelector('.bildApi')
+const pic = document.createElement("IMG");
+pic.setAttribute("src",res[0].image);
+pic.setAttribute("width", "500");
+pic.setAttribute("height", "600");
+
+bildApi.appendChild(pic);
+
+ }
+ if(input.includes("ausbildung"))
+ {
+  const test2=document.querySelector('.test2')
+  test2.style.transition="all 2s ease-in-out"
+test2.style.color="yellow"
+test2.innerHTML=res[0].ausbildung
+
+console.log(res[0].ausbildung)
+ }
+ if(input.includes("alltag"))
+ {
+  const test2=document.querySelector('.test2')
+  test2.style.transition="all 2s ease-in-out"
+test2.style.color="yellow"
+test2.innerHTML="das ist Mein Alltag"
+
+console.log(res[0].image)
+const bildApi=document.querySelector('.bildApi')
+const pic = document.createElement("IMG");
+const button=document.createElement("button")
+const text=document.createTextNode("x")
+pic.setAttribute("src",res[0].image);
+pic.setAttribute("width", "500");
+pic.setAttribute("height", "600");
+
+bildApi.appendChild(pic);
+button.appendChild(text)
+button.setAttribute("class","btn btn-primary")
+bildApi.appendChild(button);
+
+button.addEventListener("click",()=>{
+
+bildApi.remove();
+
+
+})
+
+
+ }
+
+else if(input.length===0){
+  let test2=document.querySelector('.test2')
+  
+  
+      test2.innerHTML="Bitte etwas eingeben"
+      test2.style.transition="all 2s ease-in-out"
+      test2.style.color="white"
+    
+    }
+
+// {input.length===0?setSuchTxt("suche was"):true}
 
   
 })
@@ -256,19 +387,27 @@ bildApi.remove();
 // onMouseEnter={onWort}
 
 >hier ist eine micro API über mich gespeichert einfach, die Schlagwörter 
-  wie <b>Name,Alter,Beruf,Hobby,Ausbildung,Alltag</b> etc. 
+  wie  <b> Name,Alter,Beruf,Hobby,Ausbildung,Alltag</b> etc. 
   suchen,und mit Enter bestätigen </p>
   <div>
 
 
-<input className="suchefunc br-pill h2.5 w5 dib" type="text" onKeyDown={suche} placeholder={"suchen... Eingabe(enter) dürcken"} />
-
+<input className="suchefunc br-pill h2.5 w5 dib" type="text" onKeyDown={suche} placeholder={"suchen... Eingabe(enter) dürcken"}  />
+<button onClick={()=>{
+  sucheOnClick()
+  chngBtnText()
+}} style={{
+  borderRadius:"30em",
+  background:"blue",
+  color:"#fff"
+}}
+ >{btnText}</button>
 </div>
 
 </h2>
 
 
-<h2 className="test2" ></h2>
+<h2 className="test2" >{suchTxt}</h2>
 
 
 </div>
